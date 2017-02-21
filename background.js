61c2c95887c5a14ea996fca7a82ef8c929d2b3f0
Andy McKay <amckay@mozilla.com>
2017-02-20 17:18:49 -0800

    store the listeners to provide an on and off toggle

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
 var URL = 'https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/';
+var enabled = false;
+var listeners = {};
 
-function genericLogger(msg, url, ...args) {
+var currier = function(fn) {
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  return function() {
+    return fn.apply(this, args.concat(
+      Array.prototype.slice.call(arguments, 0)));
+  };
+};
+
+function genericLogger(name, url, ...args) {
   console.group();
-  console.log(`Event fired: browser.${msg}`);
+  console.log(`Event fired: browser.${name}`);
   console.log(`Arguments: %o`, args.length === 1 ? args[0] : args);
   console.info(url);
   console.groupEnd();
@@ -18,27 +29,50 @@ function isEvent(obj) {
   return false;
 }
 
-for (let api_name of Object.keys(browser)) {
-  if (!browser[api_name]) {
-    console.log(`Skipping: ${api_name}`);
-    continue;
-  }
-  for (let method_name of Object.keys(browser[api_name])) {
-    let method = browser[api_name][method_name];
-    let name = `${api_name}.${method_name}`;
-    let url = `${URL}${api_name}/${method_name}/`;
-    if (method && isEvent(method)) {
-      if (api_name === 'webRequest') {
-        method.addListener(
-          (...args) => {genericLogger(name, url, ...args)},
-          {urls: ["<all_urls>"]}
-        );
-      } else {
-        method.addListener(
-          (...args) => {genericLogger(name, url, ...args)}
-        );
+function events(enable) {
+  for (let api_name of Object.keys(browser)) {
+    if (!browser[api_name]) {
+      console.log(`Skipping: ${api_name}`);
+      continue;
+    }
+
+    for (let method_name of Object.keys(browser[api_name])) {
+      let method = browser[api_name][method_name];
+      if (method && isEvent(method)) {
+          let name = `${api_name}.${method_name}`;
+          let url = `${URL}${api_name}/${method_name}/`;
+          if (enable) {
+            let listener = currier(currier(genericLogger, name), url);
+            listeners[url] = listener;
+            if (api_name === 'webRequest') {
+              method.addListener(listener, {urls: ["<all_urls>"]});
+            } else {
+              method.addListener(listener);
+            }
+            console.log(`Added listener to: ${name}`);
+          } else {
+            let listener = listeners[url];
+            method.removeListener(listener);
+            delete listeners[url];
+            console.log(`Removed listener from: ${name}`);
+          }
       }
-      console.log(`Added listener to: ${name}`);
     }
   }
 }
+
+function toggle() {
+  if (enabled) {
+    console.log('Turning off every event.');
+    browser.browserAction.setBadgeText({text: 'OFF'});
+    enabled = false;
+  } else {
+    console.log('Turning on every event.');
+    browser.browserAction.setBadgeText({text: 'ON'});
+    enabled = true;
+  }
+  events(enabled);
+  return enabled;
+}
+
+browser.browserAction.setBadgeText({text: 'OFF'});
--- every.js
+++ every.js
@@ -1,6 +1,23 @@
-for (let elm of document.getElementsByTagName('a')) {
+for (let elm of document.querySelectorAll('p.help a')) {
   elm.addEventListener('click', (event) => {
     browser.tabs.create({url: event.target.href});
     event.preventDefault();
   });
+}
+
+let bg = browser.extension.getBackgroundPage();
+
+for (let elm of document.querySelectorAll('p.toggle a')) {
+  elm.addEventListener('click', (event) => {
+    if (bg.toggle()) {
+      event.target.innerText = 'Turn off';
+    } else {
+      event.target.innerText = 'Turn on';
+    }
+    event.preventDefault();
+  });
+}
+
+if (bg.enabled) {
+  document.querySelector('p.toggle a').innerText = 'Turn off';
 }
