babe016bc9686990310c0592c36f8f54aec84be2
Andy McKay <amckay@mozilla.com>
2017-02-21 20:39:25 -0800

    version 2.0

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 var URL = 'https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/';
 var enabled = false;
-var listeners = {};
+var apis = {};
+var apis_grouped = {};
 
 var currier = function(fn) {
   var args = Array.prototype.slice.call(arguments, 1);
@@ -11,11 +12,10 @@ var currier = function(fn) {
   };
 };
 
-function genericLogger(name, url, ...args) {
+function genericLogger(name, ...args) {
   console.group();
   console.log(`Event fired: browser.${name}`);
   console.log(`Arguments: %o`, args.length === 1 ? args[0] : args);
-  console.info(url);
   console.groupEnd();
 }
 
@@ -29,45 +29,66 @@ function isEvent(obj) {
   return false;
 }
 
-function events(enable) {
+function populateAPIs() {
   for (let api_name of Object.keys(browser)) {
-    if (!browser[api_name]) {
-      console.log(`Skipping: ${api_name}`);
+    if (!browser[api_name] || api_name === 'test') {
       continue;
     }
-
     for (let method_name of Object.keys(browser[api_name])) {
       let method = browser[api_name][method_name];
       if (method && isEvent(method)) {
-          let name = `${api_name}.${method_name}`;
-          let url = `${URL}${api_name}/${method_name}/`;
-          if (enable) {
-            let listener = currier(currier(genericLogger, name), url);
-            listeners[url] = listener;
-            if (api_name === 'webRequest') {
-              method.addListener(listener, {urls: ["<all_urls>"]});
-            } else {
-              method.addListener(listener);
-            }
-            console.log(`Added listener to: ${name}`);
-          } else {
-            let listener = listeners[url];
-            method.removeListener(listener);
-            delete listeners[url];
-            console.log(`Removed listener from: ${name}`);
-          }
+        let name = `${api_name}.${method_name}`;
+        apis[name] = {
+          api_name: api_name,
+          method_name: method_name,
+          name: name,
+          listener: null
+        };
+        if (!Object.keys(apis_grouped).includes(api_name)) {
+          apis_grouped[api_name] = {
+            url: `${URL}${api_name}/#Events`,
+            methods: [],
+            enabled: true
+          };
+        }
+        apis_grouped[api_name].methods.push(apis[name]);
       }
     }
   }
 }
 
+function events(enable) {
+    for (let key of Object.keys(apis)) {
+      let api = apis[key];
+      let method = browser[api.api_name][api.method_name];
+      if (enable && apis_grouped[api.api_name].enabled) {
+        let listener = currier(genericLogger, api.name);
+        api.listener = listener;
+        if (api.api_name === 'webRequest') {
+          method.addListener(api.listener, {urls: ["<all_urls>"]});
+        } else {
+          method.addListener(api.listener);
+        }
+        console.log(`Added listener to: ${api.name}`);
+      } else {
+        if (api.listener) {
+          method.removeListener(api.listener);
+          api.listener = null;
+          console.log(`Removed listener from: ${api.name}`);
+        }
+      }
+    }
+}
+
 function toggle() {
   if (enabled) {
-    console.log('Turning off every event.');
+    console.log('Turning off enabled events.');
+    browser.browserAction.setBadgeBackgroundColor({color: [217, 0, 0, 255]});
     browser.browserAction.setBadgeText({text: 'OFF'});
     enabled = false;
   } else {
-    console.log('Turning on every event.');
+    console.log('Turning on enabled event.');
+    browser.browserAction.setBadgeBackgroundColor({color: 'green'});
     browser.browserAction.setBadgeText({text: 'ON'});
     enabled = true;
   }
@@ -75,4 +96,18 @@ function toggle() {
   return enabled;
 }
 
+browser.contextMenus.create({
+  id: "every-event",
+  title: "Every Event",
+  contexts: ["browser_action"]
+});
+
+function triggerAlarm() {
+  browser.alarms.create('every-event', {delayInMinutes: 0.1});
+}
+
+populateAPIs();
 browser.browserAction.setBadgeText({text: 'OFF'});
+browser.browserAction.onClicked.addListener(() => {
+  browser.tabs.create({"url": "/every.html"});
+});
--- every.js
+++ every.js
@@ -1,23 +1,75 @@
-for (let elm of document.querySelectorAll('p.help a')) {
-  elm.addEventListener('click', (event) => {
-    browser.tabs.create({url: event.target.href});
-    event.preventDefault();
-  });
-}
-
 let bg = browser.extension.getBackgroundPage();
+
+function toggle(target) {
+  if (bg.toggle()) {
+    target.innerText = 'Turn off';
+    target.className = 'btn btn-danger';
+  } else {
+    target.innerText = 'Turn on';
+    target.className = 'btn btn-success';
+  }
+}
 
 for (let elm of document.querySelectorAll('p.toggle a')) {
   elm.addEventListener('click', (event) => {
-    if (bg.toggle()) {
-      event.target.innerText = 'Turn off';
-    } else {
-      event.target.innerText = 'Turn on';
-    }
+    toggle(event.target);
     event.preventDefault();
   });
 }
 
 if (bg.enabled) {
   document.querySelector('p.toggle a').innerText = 'Turn off';
+}
+
+let root = document.getElementsByClassName('apis')[0];
+
+for (let key of Object.keys(bg.apis_grouped)) {
+  let api = bg.apis_grouped[key];
+  let row = document.createElement('div');
+  let input = document.createElement('input');
+  input.type = 'checkbox';
+  input.checked = api.enabled;
+  input.value = key;
+  let text = document.createTextNode(` ${key}: ${api.methods.length} event(s) | `);
+  let url = document.createElement('a');
+  url.href = api.url;
+  url.innerText = 'docs';
+  url.className = 'external';
+  row.appendChild(input);
+  row.appendChild(text);
+  row.appendChild(url);
+  root.appendChild(row);
+}
+
+for (let elm of document.getElementsByClassName('external')) {
+  elm.addEventListener('click', (event) => {
+    browser.tabs.create({url: event.target.href});
+    event.preventDefault();
+  });
+}
+
+for (let elm of document.getElementsByClassName('alarm')) {
+  elm.addEventListener('click', (event) => {
+    bg.triggerAlarm();
+    event.preventDefault();
+  });
+}
+
+for (let elm of document.getElementsByClassName('label')) {
+  elm.addEventListener('click', (event) => {
+    let state = event.target.dataset.action == "on" ? true : false;
+    for (let input of document.querySelectorAll('input[type=checkbox]')) {
+      input.checked = state;
+      bg.apis_grouped[input.value].enabled = state;
+    }
+    event.preventDefault();
+  });
+}
+
+for (let elm of document.querySelectorAll('input[type=checkbox]')) {
+  elm.addEventListener('change', (event) => {
+    bg.apis_grouped[event.target.value].enabled = event.target.checked;
+    console.log(`changed ${event.target.value}`);
+    event.preventDefault();
+  });
 }
